d0b16fe1fe762715e8a133d5704c4ccaaf688010
braverokmc79 <braverokmc79@gmail.com>
2022-09-28 20:50:56 +0900

    18. 라우터 - 파일로 분리 1

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const template = require('./lib/template.js');
-const path = require('path');
-const sanitizeHtml = require('sanitize-html');
 const fs = require('fs');
 const bodyParser = require('body-parser')
 const compression = require('compression')
+const topicRouter = require("./routes/topic");
+
 const app = express()
 const port = 3000
 
@@ -18,6 +18,8 @@ app.get('*', (req, res, next) => {
   });
 
 });
+
+app.use('/topic', topicRouter);
 
 
 
@@ -34,115 +36,10 @@ app.get('/', (req, res) => {
   res.send(html);
 })
 
-app.get("/topic/create", (req, res) => {
-
-  const title = 'WEB - create';
-  const list = template.list(req.list);
-  const html = template.HTML(title, list, `
-            <form action="/topic/create_process" method="post">
-              <p><input type="text" name="title" placeholder="title"></p>
-              <p>
-                <textarea name="description" placeholder="description"></textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-          `, '');
-  res.send(html);
-
-});
-
-
-app.post("/topic/create_process", (req, res) => {
-  const post = req.body;
-  const title = post.title;
-  const description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-    res.redirect(`/topic/${title}`);
-  });
-
-});
 
 
 
-app.get("/topic/update/:pageId", (req, res) => {
-  const filteredId = path.parse(req.params.pageId).base;
 
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    const title = filteredId;
-    const list = template.list(req.list);
-    const html = template.HTML(title, list,
-      `
-              <form action="/topic/update_process" method="post">
-                <input type="hidden" name="id" value="${title}">
-                <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-                <p>
-                  <textarea name="description" placeholder="description">${description}</textarea>
-                </p>
-                <p>
-                  <input type="submit">
-                </p>
-              </form>
-              `,
-      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
-    );
-    res.send(html);
-  });
-
-});
-
-
-app.post("/topic/update_process", (req, res) => {
-  const post = req.body;
-  const id = post.id;
-  const title = post.title;
-  const description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function (error) {
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-      res.redirect(`/topic/${title}`);
-    })
-  });
-
-})
-
-
-app.post("/topic/delete_process", (req, res) => {
-  const post = req.body;
-  const id = post.id;
-  const filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function (error) {
-    res.redirect("/");
-  })
-});
-
-
-app.get('/topic/:pageId', (req, res, next) => {
-
-  const filteredId = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    if (err) return next(err);
-
-    const title = filteredId;
-    const sanitizedTitle = sanitizeHtml(title);
-    const sanitizedDescription = sanitizeHtml(description, {
-      allowedTags: ['h1']
-    });
-    const list = template.list(req.list);
-    const html = template.HTML(sanitizedTitle, list,
-      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-      ` <a href="/topic/create">create</a>
-                  <a href="/topic/update/${sanitizedTitle}">update</a>
-                  <form action="/topic/delete_process" method="post">
-                    <input type="hidden" name="id" value="${sanitizedTitle}">
-                    <input type="submit" value="delete">
-                  </form>`
-    );
-    res.send(html);
-
-
-  });
-});
 
 
 
