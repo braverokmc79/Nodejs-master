188bd4c0ab9ed4b6f87e49d09b0da76e1d3e92ec
braverokmc79 <braverokmc79@gmail.com>
2022-09-28 13:37:46 +0900

    5. Hello world 2

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const template = require('./lib/template.js')
+const fs = require('fs');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+  fs.readdir('./data', function (error, filelist) {
+    const title = 'Welcome';
+    const description = 'Hello, Node.js';
+    const list = template.list(filelist);
+    const html = template.HTML(title, list,
+      `<h2>${title}</h2>${description}`,
+      `<a href="/create">create</a>`
+    );
+    res.send(html);
+  });
+
 })
 
 
