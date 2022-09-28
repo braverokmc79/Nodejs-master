e9e090c4fad1353b0decb741cddb304f188ac1a7
braverokmc79 <braverokmc79@gmail.com>
2022-09-28 13:30:05 +0900

    4. Hello world 2

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/page', (req, res) => {
+  res.send("/page");
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
+
+
 // var http = require('http');
 // var fs = require('fs');
 // var url = require('url');
@@ -142,14 +166,3 @@
 // });
 // app.listen(3000);
 
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
