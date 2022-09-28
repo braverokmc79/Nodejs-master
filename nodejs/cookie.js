78f778b9fcd377e8256f893fecaaeb207a1f1091
braverokmc79 <braverokmc79@gmail.com>
2022-09-28 22:09:13 +0900

    27. 쿠키읽기

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,6 +1,14 @@
 const http = require('http');
-http.createServer(function (request, response) {
+const cookie = require("cookie");
 
+http.createServer(function (request, response) {
+    console.log(request.headers.cookie);
+    let cookies = {};
+    if (request.headers.cookie !== undefined) {
+        cookies = cookie.parse(request.headers.cookie);
+    }
+
+    console.log("cookies : ", cookies);
     response.writeHead(200, {
         'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
     });
