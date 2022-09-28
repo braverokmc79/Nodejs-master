41c6af9dfad78d69fc9df1318732d3348c33a18f
braverokmc79 <braverokmc79@gmail.com>
2022-09-28 20:19:25 +0900

    17. 라우터 - 주소체계변경

--- main.js
+++ main.js
@@ -29,48 +29,17 @@ app.get('/', (req, res) => {
     `<h2>${title}</h2>${description}   
      <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px" >
     `,
-    `<a href="/create">create</a>`
+    `<a href="/topic/create">create</a>`
   );
   res.send(html);
 })
 
-
-app.get('/page/:pageId', (req, res, next) => {
-
-  const filteredId = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    if (err) return next(err);
-
-    const title = filteredId;
-    const sanitizedTitle = sanitizeHtml(title);
-    const sanitizedDescription = sanitizeHtml(description, {
-      allowedTags: ['h1']
-    });
-    const list = template.list(req.list);
-    const html = template.HTML(sanitizedTitle, list,
-      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-      ` <a href="/create">create</a>
-                  <a href="/update/${sanitizedTitle}">update</a>
-                  <form action="/delete_process" method="post">
-                    <input type="hidden" name="id" value="${sanitizedTitle}">
-                    <input type="submit" value="delete">
-                  </form>`
-    );
-    res.send(html);
-
-
-  });
-
-
-});
-
-
-app.get("/create", (req, res) => {
+app.get("/topic/create", (req, res) => {
 
   const title = 'WEB - create';
   const list = template.list(req.list);
   const html = template.HTML(title, list, `
-            <form action="/create_process" method="post">
+            <form action="/topic/create_process" method="post">
               <p><input type="text" name="title" placeholder="title"></p>
               <p>
                 <textarea name="description" placeholder="description"></textarea>
@@ -84,20 +53,20 @@ app.get("/create", (req, res) => {
 
 });
 
-app.post("/create_process", (req, res) => {
+
+app.post("/topic/create_process", (req, res) => {
   const post = req.body;
   const title = post.title;
   const description = post.description;
   fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-    res.redirect(`/page/${title}`);
+    res.redirect(`/topic/${title}`);
   });
 
-})
+});
 
 
-app.get("/update/:pageId", (req, res) => {
 
-
+app.get("/topic/update/:pageId", (req, res) => {
   const filteredId = path.parse(req.params.pageId).base;
 
   fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
@@ -105,7 +74,7 @@ app.get("/update/:pageId", (req, res) => {
     const list = template.list(req.list);
     const html = template.HTML(title, list,
       `
-              <form action="/update_process" method="post">
+              <form action="/topic/update_process" method="post">
                 <input type="hidden" name="id" value="${title}">
                 <p><input type="text" name="title" placeholder="title" value="${title}"></p>
                 <p>
@@ -116,37 +85,63 @@ app.get("/update/:pageId", (req, res) => {
                 </p>
               </form>
               `,
-      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
     );
     res.send(html);
   });
 
-
-
 });
 
 
-app.post("/update_process", (req, res) => {
+app.post("/topic/update_process", (req, res) => {
   const post = req.body;
   const id = post.id;
   const title = post.title;
   const description = post.description;
   fs.rename(`data/${id}`, `data/${title}`, function (error) {
     fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-      res.redirect(`/page/${title}`);
+      res.redirect(`/topic/${title}`);
     })
   });
 
 })
 
 
-app.post("/delete_process", (req, res) => {
+app.post("/topic/delete_process", (req, res) => {
   const post = req.body;
   const id = post.id;
   const filteredId = path.parse(id).base;
   fs.unlink(`data/${filteredId}`, function (error) {
     res.redirect("/");
   })
+});
+
+
+app.get('/topic/:pageId', (req, res, next) => {
+
+  const filteredId = path.parse(req.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+    if (err) return next(err);
+
+    const title = filteredId;
+    const sanitizedTitle = sanitizeHtml(title);
+    const sanitizedDescription = sanitizeHtml(description, {
+      allowedTags: ['h1']
+    });
+    const list = template.list(req.list);
+    const html = template.HTML(sanitizedTitle, list,
+      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+      ` <a href="/topic/create">create</a>
+                  <a href="/topic/update/${sanitizedTitle}">update</a>
+                  <form action="/topic/delete_process" method="post">
+                    <input type="hidden" name="id" value="${sanitizedTitle}">
+                    <input type="submit" value="delete">
+                  </form>`
+    );
+    res.send(html);
+
+
+  });
 });
 
 
