35ce7bebd9222bd5d98767930fac8eb155e4633e
braverokmc79 <braverokmc79@gmail.com>
2022-09-29 00:33:09 +0900

    30. 쿠키 옵션 - Secure & HttpOnly

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -10,8 +10,12 @@ http.createServer(function (request, response) {
 
     console.log("cookies : ", cookies);
     response.writeHead(200, {
-        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry',
-            `Permanent=cookies; Max-Age=${60*60*24*30}`
+        'Set-Cookie': [
+            'yummy_cookie=choco',
+            'tasty_cookie=strawberry',
+            `Permanent=cookies; Max-Age=${60 * 60 * 24 * 30}`,
+            'Secure=Secure; Secure',
+            'HttpOnly=HttpOnly; HttpOnly'
         ]
     });
 
