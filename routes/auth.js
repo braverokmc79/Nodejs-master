195c00af4af4d4fc10b4971bdd76b6109160303a
braverokmc79 <braverokmc79@gmail.com>
2022-09-30 01:45:46 +0900

    45. 인증구현 - 로그인 세션구현

--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,13 @@ const path = require('path');
 const sanitizeHtml = require('sanitize-html');
 const fs = require('fs');
 
+const authData = {
+  email: "egoing777@gmail.com",
+  password: "1111",
+  nickanme: 'macaronics'
+}
 
 router.get("/login", (req, res) => {
-
   const title = 'WEB - login';
   const list = template.list(req.list);
   const html = template.HTML(title, list, `
@@ -21,6 +25,21 @@ router.get("/login", (req, res) => {
           `, '');
   res.send(html);
 
+});
+
+
+router.post("/login_process", (req, res) => {
+  const post = req.body;
+  const email = post.email;
+  const password = post.password;
+
+  if (email === authData.email && password === authData.password) {
+
+    res.send("Welcome!");
+    //res.redirect("/");
+  } else {
+    res.send("Who?");
+  }
 });
 
 
