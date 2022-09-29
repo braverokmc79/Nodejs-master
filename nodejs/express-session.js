814d110d2f09d4973f4b96fad15ce1ba6a47e4c1
braverokmc79 <braverokmc79@gmail.com>
2022-09-29 23:35:08 +0900

    41. express-session의 옵션

--- nodejs/express-session.js
+++ nodejs/express-session.js
@@ -10,26 +10,12 @@ app.use(session({
     saveUninitialized: true
 }))
 
-app.use(function (req, res, next) {
-    if (!req.session.views) {
-        req.session.views = {}
-    }
 
-    // get the url pathname
-    var pathname = parseurl(req).pathname
-
-    // count the views
-    req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
-
-    next()
+app.get('/', function (req, res, next) {
+    res.send('Hello session')
 })
 
-app.get('/foo', function (req, res, next) {
-    res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
-})
 
-app.get('/bar', function (req, res, next) {
-    res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
-})
-
-app.listen(3000);
+app.listen(3000, function () {
+    console.log("3000!");
+});
